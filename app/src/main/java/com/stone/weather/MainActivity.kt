2ca4aadd3a1b9e66f3b7516c885547acef4beb80
ThantSin17 <skybluestone7@gmail.com>
2022-01-31 21:03:35 +0630

    update UI

--- app/src/main/java/com/stone/weather/MainActivity.kt
+++ app/src/main/java/com/stone/weather/MainActivity.kt
@@ -12,51 +12,51 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    private val textview by lazy {
-        findViewById<TextView>(R.id.textview)
-    }
-    private val editText by lazy {
-        findViewById<EditText>(R.id.edit)
-    }
-    private val button by lazy {
-        findViewById<Button>(R.id.btn)
-    }
-    private val retrofit by lazy {
-        RetrofitApi().instance()
-    }
+//    private val textview by lazy {
+//        findViewById<TextView>(R.id.textview)
+//    }
+//    private val editText by lazy {
+//        findViewById<EditText>(R.id.edit)
+//    }
+//    private val button by lazy {
+//        findViewById<Button>(R.id.btn)
+//    }
+//    private val retrofit by lazy {
+//        RetrofitApi().instance()
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.test)
 
-        button.setOnClickListener {
-            val zip=editText.text.toString()
-            executeNetworkCall(zip)
-        }
+//        button.setOnClickListener {
+//            val zip=editText.text.toString()
+//            executeNetworkCall(zip)
+//        }
     }
 
-    private fun executeNetworkCall(zip: String) {
-        val api=retrofit.create(ApiService::class.java)
-        api.getWeatherList(zip).enqueue(object : Callback<WeatherResponse>{
-                override fun onResponse(
-                    call: Call<WeatherResponse>,
-                    response: Response<WeatherResponse>
-                ) {
-                    if (response.isSuccessful){
-                        Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
-                        textview.text=response.body()?.cityName.toString()
-                    }else{
-                        Toast.makeText(applicationContext,"fail",Toast.LENGTH_SHORT).show()
-
-                    }
-
-                }
-
-                override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                    Log.i("mainLog",t.message.toString())
-                    Toast.makeText(applicationContext,"fail",Toast.LENGTH_SHORT).show()
-
-                }
-            })
-    }
+//    private fun executeNetworkCall(zip: String) {
+//        val api=retrofit.create(ApiService::class.java)
+//        api.getWeatherList(zip).enqueue(object : Callback<WeatherResponse>{
+//                override fun onResponse(
+//                    call: Call<WeatherResponse>,
+//                    response: Response<WeatherResponse>
+//                ) {
+//                    if (response.isSuccessful){
+//                        Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
+//                        textview.text=response.body()?.cityName.toString()
+//                    }else{
+//                        Toast.makeText(applicationContext,"fail",Toast.LENGTH_SHORT).show()
+//
+//                    }
+//
+//                }
+//
+//                override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+//                    Log.i("mainLog",t.message.toString())
+//                    Toast.makeText(applicationContext,"fail",Toast.LENGTH_SHORT).show()
+//
+//                }
+//            })
+//    }
 }
